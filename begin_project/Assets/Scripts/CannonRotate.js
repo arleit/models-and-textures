09615e84c5afd84ce8770f9cd6e3ca8797aa0b0a
flaherja <james.flaherty@student.shu.edu>
2015-05-27 20:24:44 -0400

    please work dear god

---
+++ begin_project/Assets/Scripts/CannonRotate.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+transform.Rotate(-Input.GetAxis("Mouse Y"),0,0);
+}
---
+++ begin_project/Assets/Scripts/shittyControllerIMade.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+if (Input.GetKey (KeyCode.W))
+{
+	transform.Translate(0,0,1);
+}
+if (Input.GetKey (KeyCode.S))
+{
+	transform.Translate(0,0,-1);
+}
+if (Input.GetKey (KeyCode.D))
+{
+	transform.Rotate(0,1,0);
+}
+if (Input.GetKey (KeyCode.A))
+{
+	transform.Rotate(0,-1,0);
+}
+}
